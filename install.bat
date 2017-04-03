f159593b1698b26833578ae1886282a409e98a38
donmahallem <m@m.m0k.eu>
2017-04-03 14:38:33 +0200

    init

---
+++ install.bat
@@ -0,0 +1,10 @@
+git clone https://github.com/DonMahallem/got-api.git
+git clone https://github.com/DonMahallem/got-crawler.git
+cd got-api
+npm install
+npm run build
+cd ..
+cd got-crawler
+npm install
+npm run build
+cd ..
